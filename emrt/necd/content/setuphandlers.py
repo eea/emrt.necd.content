fe77696c69409206a781f6375751a05c8891c88e
David Batranu <david.batranu@eaudeweb.ro>
2017-02-15 20:46:41 +0200

    [refs #82126] Moving setup to post_install hook.
    Installing plone.app.ldap so the ldap-plugin gets properly setup in the
    PAS authentication workflow.

--- emrt/necd/content/setuphandlers.py
+++ emrt/necd/content/setuphandlers.py
@@ -1,7 +1,15 @@
+import logging
+
+from zope.component.hooks import getSite
+
 from Products.CMFCore.utils import getToolByName
 from Products.ATVocabularyManager.config import SORT_METHOD_FOLDER_ORDER
+
 from emrt.necd.content.constants import LDAP_SECTOREXP
 from emrt.necd.content.constants import LDAP_SECRETARIAT
+
+
+LOGGER = logging.getLogger('emrt.necd.content.setuphandlers')
 
 
 VOCABULARIES = [
@@ -38,31 +46,6 @@ VOCABULARIES = [
      'filename': 'conclusion_reasons.csv',
     },
 ]
-
-
-LDAP_SETTINGS = {
-    'id': 'ldap-plugin',
-    'title': 'LDAP',
-    'login_attr': 'uid',
-    'uid_attr': 'uid',
-    'users_base': 'ou=Users,o=EIONET,l=Europe',
-    'users_scope': 2,
-    'roles': 'Authenticated',
-    'groups_base': (
-        'cn=extranet-necd-review,'
-        'cn=extranet-necd,'
-        'cn=extranet,'
-        'ou=Roles,o=EIONET,l=Europe'
-    ),
-    'groups_scope': 2,
-    'binduid': '',
-    'bindpwd': '',
-    'binduid_usage': 0,
-    'LDAP_server': 'ldap3.eionet.europa.eu',
-    'rdn_attr': 'uid',
-    'local_groups': 0,
-    'read_only': True,
-}
 
 
 LDAP_ROLE_MAPPING = {
@@ -119,30 +102,35 @@ def enable_atd_spellchecker(portal):
     tinymce.libraries_atd_service_url = u'service.afterthedeadline.com'
 
 
-def setup_memcached(portal):
+def setup_memcached(portal, memcached_id):
     if 'memcached' not in portal.keys():
         try:
-            oid = portal.manage_addProduct[
-                'MemcachedManager'].manage_addMemcachedManager(MEMCACHED_ID)
+            _ = portal.manage_addProduct[
+                'MemcachedManager'].manage_addMemcachedManager(memcached_id)
         except Exception, err:
-            logger.exception(err)
+            LOGGER.exception(err)
         else:
-            cache = portal[MEMCACHED_ID]
+            cache = portal[memcached_id]
             cache._settings['servers'] = ('127.0.0.1:11211', )
             cache._p_changed = True
 
 
-def setup_ldap(portal):
+def setup_ldap(portal, ldap_id, memcached_id):
     acl = portal['acl_users']
-    acl.manage_addProduct['PloneLDAP'].manage_addPloneLDAPMultiPlugin(
-        **LDAP_SETTINGS)
-    ldap_plugin = acl[LDAP_SETTINGS['id']]
+    ldap_plugin = acl[ldap_id]
     ldap_acl = ldap_plugin._getLDAPUserFolder()
     for ldap_group, plone_role in LDAP_ROLE_MAPPING.items():
         ldap_acl.manage_addGroupMapping(ldap_group, plone_role)
 
-    # enable memcache
-    ldap_plugin.ZCacheable_setManagerId(manager_id=MEMCACHED_ID)
+    # enable memcached
+    ldap_plugin.ZCacheable_setManagerId(manager_id=memcached_id)
+
+
+def post_install(context):
+    memcached_id = 'memcached'
+    portal = getSite()
+    setup_memcached(portal, memcached_id)
+    setup_ldap(portal, 'ldap-plugin', memcached_id)
 
 
 def setupVarious(context):
@@ -154,5 +142,3 @@ def setupVarious(context):
 
     prepareVocabularies(portal, context)
     enable_atd_spellchecker(portal)
-    setup_memcached(portal)
-    setup_ldap(portal)
