1b3226241a7962944daa5366ae70e5a71e12e28f
David Batranu <david.batranu@eaudeweb.ro>
2017-06-08 18:22:46 +0300

    [refs #85805] Faster create observation.
    Only reindex security if the roles have actually changed.

--- emrt/necd/content/roles/localrolesubscriber.py
+++ emrt/necd/content/roles/localrolesubscriber.py
@@ -17,14 +17,16 @@ def grant_local_roles(context):
     """
     country = context.country.lower()
     sector = context.ghg_source_category_value()
-    applyes_to = [context]
+    applies_to = [context]
     parent = aq_parent(aq_inner(context))
     if IReviewFolder.providedBy(parent):
-        applyes_to.append(parent)
+        applies_to.append(parent)
 
     context.__ac_local_roles_block__ = True
 
-    for obj in applyes_to:
+    for obj in applies_to:
+        _roles_start = obj.get_local_roles()
+
         api.group.grant_roles(
             groupname='{}-{}-{}'.format(LDAP_SECTOREXP, sector, country),
             roles=[ROLE_SE],
@@ -40,4 +42,8 @@ def grant_local_roles(context):
             roles=[ROLE_MSA],
             obj=obj,
         )
-        obj.reindexObjectSecurity()
+
+        _roles_end = obj.get_local_roles()
+        # Reindex only if roles were changed.
+        if _roles_end != _roles_start:
+            obj.reindexObjectSecurity()
