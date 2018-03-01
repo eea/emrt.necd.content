774830808965b7ff7ef0f8f8dd2a35433c7e24ae
Irina Botez <irina.botez@eaudeweb.ro>
2018-03-01 18:43:52 +0200

    [refs #91903] Created dynamic xls file generated from vocabulary values

--- emrt/necd/content/browser/get_sample_xls.py
+++ emrt/necd/content/browser/get_sample_xls.py
@@ -2,43 +2,53 @@ from Products.Five.browser import BrowserView
 from StringIO import StringIO
 
 from openpyxl import Workbook
-from openpyxl.styles.borders import Border, Side
+
+from plone import api
 
 XLS_SAMPLE_HEADER = ['Observation description', 'Country', 'NFR Code',
                      'Inventory Year', 'Pollutants', 'Review Year', 'Parameter']
 
+DESC = 'Description of the observation'
+NFR_CODE = '1A1'
+INVENTORY_YEAR = '2017'
+REVIEW_YEAR = '2017'
+
 
 class GetSampleXLS(BrowserView):
-
-    def set_border(self, sheet, max_row, max_col):
-
-        medium_border = Border(left=Side(style='medium'),
-                               right=Side(style='medium'),
-                               top=Side(style='medium'),
-                               bottom=Side(style='medium'))
-
-        for row in range(1,max_row+1):
-            for col in range(1,max_col+1):
-                sheet.cell(row=row, column=col).border = medium_border
-
 
     def populate_cells(self, sheet):
 
         sheet.append(XLS_SAMPLE_HEADER)
 
-        sheet['A2'] = 'Here comes the short description of the observation'
-        sheet['C2'] = '1A1'
-        sheet['D2'] = '2017'
-        sheet['F2'] = '2018'
+        pvoc = api.portal.get_tool('portal_vocabularies')
+        country_voc = pvoc.getVocabularyByName('eea_member_states')
+        pollutants_voc = pvoc.getVocabularyByName('pollutants')
+        parameter_voc = pvoc.getVocabularyByName('parameter')
 
-        self.set_border(sheet, sheet.max_row, sheet.max_column)
+        countries = [term.title for term in country_voc.values()]
+        pollutants = '\n'.join([term.title for term in pollutants_voc.values()])
+        parameter = '\n'.join([term.title for term in parameter_voc.values()])
 
+        for country in countries:
+            row = [DESC, country, NFR_CODE, INVENTORY_YEAR, pollutants,
+                   REVIEW_YEAR, parameter]
+            sheet.append(row)
 
     def __call__(self):
         wb = Workbook()
         sheet = wb.create_sheet('Observation', 0)
 
         self.populate_cells(sheet)
+
+        #wrap text for multi line cells
+        for row in sheet.iter_rows():
+            for cell in row:
+                cell.alignment = cell.alignment.copy(wrapText=True)
+
+        #set cell max width
+        for column_cells in sheet.columns:
+            length = max(len(str(cell.value)) for cell in column_cells)
+            sheet.column_dimensions[column_cells[0].column].width = length
 
         xls = StringIO()
 
