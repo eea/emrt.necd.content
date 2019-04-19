292a266e2f93b6daddd8f6c130b5cb4a7a0c1ac2
David Batranu <david.batranu@eaudeweb.ro>
2019-04-19 18:53:37 +0300

    [refs #103622] (WIP) - Ensure correct state

--- emrt/necd/content/browser/carryover.py
+++ emrt/necd/content/browser/carryover.py
@@ -4,6 +4,8 @@ from functools import partial
 
 from zope.component.hooks import getSite
 
+from DateTime import DateTime
+
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -60,34 +62,47 @@ def prepend_qa(target, source):
         _copy_obj(target, source_qa)
 
 
-def copy_direct(context, rows):
-    site_url = getSite().absolute_url()
-    obj_from_url = partial(_obj_from_url, context, site_url)
-    catalog = getToolByName(context, 'portal_catalog')
+def add_to_wh(wf, obj, action, state):
+    wh = obj.workflow_history
+    wf_id = wf.getId()
+    wh[wf_id] = wh[wf_id] + {
+        'comments': 'Carryover force state',
+        'actor': '',
+        'time': DateTime(),
+        'action': action,
+        'review_state': state,
+    }
+    wf.updateRoleMappingsFor(obj)
 
-    for row in rows:
-        source = _read_col(row, 0)
-        obj = obj_from_url(source)
-        ob = _copy_and_flag(context, obj)
-        catalog.catalog_object(ob)
+
+def reopen_with_qa(wf, wf_q, obj):
+    add_to_wh(wf, obj, 'reopen-qa-chat', 'pending')
+    question = obj.get_question()
+    if question:
+        add_to_wh(wf_q, obj, 'reopen', 'draft')
 
 
-def copy_complex(context, rows):
-    site_url = getSite().absolute_url()
-    obj_from_url = partial(_obj_from_url, context, site_url)
-    catalog = getToolByName(context, 'portal_catalog')
+def copy_direct(context, catalog, wf, wf_q, obj_from_url, row):
+    source = _read_col(row, 0)
+    obj = obj_from_url(source)
+    ob = _copy_and_flag(context, obj)
+    reopen_with_qa(wf, wf_q, ob)
+    catalog.catalog_object(ob)
 
-    for row in rows:
-        source = _read_col(row, 0)
-        older_source = _read_col(row, 1)
 
-        obj = obj_from_url(source)
-        older_obj = obj_from_url(older_source)
+def copy_complex(context, catalog, wf, wf_q, obj_from_url, row):
+    source = _read_col(row, 0)
+    older_source = _read_col(row, 1)
 
-        ob = _copy_and_flag(context, obj, older_obj.getId())
+    obj = obj_from_url(source)
+    older_obj = obj_from_url(older_source)
 
-        prepend_qa(ob, older_obj)
-        catalog.catalog_object(ob)
+    ob = _copy_and_flag(context, obj, older_obj.getId())
+
+    prepend_qa(ob, older_obj)
+    reopen_with_qa(wf, wf_q, ob)
+
+    catalog.catalog_object(ob)
 
 
 class CarryOverView(BrowserView):
@@ -105,7 +120,20 @@ class CarryOverView(BrowserView):
         valid_rows = tuple(takewhile(
             lambda row: any(c.value for c in row), sheet.rows))[1:]
 
-        if action == 'direct':
-            copy_direct(self.context, valid_rows)
-        elif action == 'complex':
-            copy_complex(self.context, valid_rows)
+        context = self.context
+        site_url = getSite().absolute_url()
+        obj_from_url = partial(_obj_from_url, context, site_url)
+        catalog = getToolByName(context, 'portal_catalog')
+        wft = getToolByName(context, 'portal_workflow')
+        # import pdb; pdb.set_trace()
+        wf_obs = wft.getWorkflowbyId(wft.getChainFor('Observation'))
+        wf_question = wft.getWorkflowbyId(wft.getChainFor('Question'))
+
+        actions = dict(direct=copy_direct, complex=copy_complex)
+        copy_func = partial(
+            actions[action],
+            context, catalog, wf_obs, wf_question, obj_from_url
+        )
+
+        for row in valid_rows:
+            copy_func(row)
