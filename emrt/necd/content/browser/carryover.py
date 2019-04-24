f63627f372c856e6baaedfcc4dd5afd54cbf5cc1
David Batranu <david.batranu@eaudeweb.ro>
2019-04-24 20:13:00 +0300

    [refs #103622] Completing carry-over script.
    * Ensure review_year.
    * Ensure workflow states.
    * Update highlight vocabulary.
    * Add button for carryover.
    * Use main_template for carryover form.

--- emrt/necd/content/browser/carryover.py
+++ emrt/necd/content/browser/carryover.py
@@ -10,6 +10,7 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from Products.CMFCore.utils import getToolByName
+from Products.statusmessages.interfaces import IStatusMessage
 
 import openpyxl
 
@@ -18,7 +19,8 @@ LOG = getLogger('emrt.necd.content.carryover')
 
 
 def _read_col(row, nr):
-    return row[nr].value.strip()
+    val = row[nr].value
+    return val.strip() if val else val
 
 
 def _copy_obj(target, ob, new_id=None):
@@ -34,6 +36,7 @@ def _copy_and_flag(context, obj, new_id=None):
     _, _, year, index = (new_id or obj.getId()).split('-')
     ob = _copy_obj(context, obj, new_id=new_id)
     ob.carryover_from = year
+    ob.review_year = int(year)
 
     LOG.info(
         'Copied %s -> %s',
@@ -49,15 +52,22 @@ def _obj_from_url(context, site_url, url):
     return context.unrestrictedTraverse(traversable)
 
 
+def replace_conclusion_text(obj, text):
+    conclusion = obj.get_conclusion()
+    if text and conclusion:
+        conclusion.text = text
+
+
 def prepend_qa(target, source):
     source_qa = source.get_question()
     target_qa = target.get_question()
 
     if source_qa and target_qa:
+        ordering = target_qa.getOrdering()
         for comment in source_qa.values():
             _copy_obj(target_qa, comment)
-            target.notifyAdded(comment.getId())
-        target_qa.orderObjects(key='creation_date')
+            ordering.notifyAdded(comment.getId())
+        ordering.orderObjects(key='creation_date')
     elif source_qa and not target_qa:
         _copy_obj(target, source_qa)
 
@@ -65,42 +75,53 @@ def prepend_qa(target, source):
 def add_to_wh(wf, obj, action, state):
     wh = obj.workflow_history
     wf_id = wf.getId()
-    wh[wf_id] = wh[wf_id] + {
+    wh[wf_id] = wh[wf_id] + ({
         'comments': 'Carryover force state',
         'actor': '',
         'time': DateTime(),
         'action': action,
         'review_state': state,
-    }
+    }, )
     wf.updateRoleMappingsFor(obj)
 
 
-def reopen_with_qa(wf, wf_q, obj):
+def reopen_with_qa(wf, wf_q, wf_c, obj):
     add_to_wh(wf, obj, 'reopen-qa-chat', 'pending')
     question = obj.get_question()
     if question:
-        add_to_wh(wf_q, obj, 'reopen', 'draft')
+        add_to_wh(wf_q, question, 'reopen', 'draft')
+
+    conclusion = obj.get_conclusion()
+    if conclusion:
+        add_to_wh(wf_c, conclusion, 'redraft', 'draft')
 
 
-def copy_direct(context, catalog, wf, wf_q, obj_from_url, row):
+def copy_direct(context, catalog, wf, wf_q, wf_c, obj_from_url, row):
     source = _read_col(row, 0)
+    conclusion_text = _read_col(row, 1)
+
     obj = obj_from_url(source)
     ob = _copy_and_flag(context, obj)
-    reopen_with_qa(wf, wf_q, ob)
+
+    replace_conclusion_text(ob, conclusion_text)
+    reopen_with_qa(wf, wf_q, wf_c, ob)
+
     catalog.catalog_object(ob)
 
 
-def copy_complex(context, catalog, wf, wf_q, obj_from_url, row):
+def copy_complex(context, catalog, wf, wf_q, wf_c, obj_from_url, row):
     source = _read_col(row, 0)
     older_source = _read_col(row, 1)
+    conclusion_text = _read_col(row, 2)
 
     obj = obj_from_url(source)
     older_obj = obj_from_url(older_source)
 
     ob = _copy_and_flag(context, obj, older_obj.getId())
 
+    replace_conclusion_text(ob, conclusion_text)
     prepend_qa(ob, older_obj)
-    reopen_with_qa(wf, wf_q, ob)
+    reopen_with_qa(wf, wf_q, wf_c, ob)
 
     catalog.catalog_object(ob)
 
@@ -113,6 +134,7 @@ class CarryOverView(BrowserView):
         return self.index()
 
     def start(self, action, xls):
+        portal = getSite()
         wb = openpyxl.load_workbook(xls, read_only=True, data_only=True)
         sheet = wb.worksheets[0]
 
@@ -121,19 +143,26 @@ class CarryOverView(BrowserView):
             lambda row: any(c.value for c in row), sheet.rows))[1:]
 
         context = self.context
-        site_url = getSite().absolute_url()
+        site_url = portal.absolute_url()
         obj_from_url = partial(_obj_from_url, context, site_url)
-        catalog = getToolByName(context, 'portal_catalog')
-        wft = getToolByName(context, 'portal_workflow')
-        # import pdb; pdb.set_trace()
-        wf_obs = wft.getWorkflowbyId(wft.getChainFor('Observation'))
-        wf_question = wft.getWorkflowbyId(wft.getChainFor('Question'))
+        catalog = getToolByName(portal, 'portal_catalog')
+        wft = getToolByName(portal, 'portal_workflow')
+
+        wf_obs = wft.getWorkflowById(wft.getChainFor('Observation')[0])
+        wf_question = wft.getWorkflowById(wft.getChainFor('Question')[0])
+        wf_conclusion = wft.getWorkflowById(wft.getChainFor('Conclusions')[0])
 
         actions = dict(direct=copy_direct, complex=copy_complex)
         copy_func = partial(
             actions[action],
-            context, catalog, wf_obs, wf_question, obj_from_url
+            context, catalog,
+            wf_obs, wf_question, wf_conclusion,
+            obj_from_url
         )
 
         for row in valid_rows:
             copy_func(row)
+
+        (IStatusMessage(self.request)
+            .add('Carryover successfull!', type='info'))
+        self.request.RESPONSE.redirect(context.absolute_url())
