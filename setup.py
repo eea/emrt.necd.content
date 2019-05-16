d7a8bf60390295795da5f41f929fc57e2fcb7f0a
David Batranu <david.batranu@eaudeweb.ro>
2019-05-16 18:06:53 +0300

    [refs #88845] Release 2.3.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.errors import DistutilsExecError
 from distutils.errors import DistutilsPlatformError
 import os
 
-VERSION = '2.3.8'
+VERSION = '2.3.9'
 
 
 class BuildFailed(Exception):
