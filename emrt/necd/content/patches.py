bded054c4df9da9a01421a9bf29cac37bab56dc0
David Batranu <david.batranu@eaudeweb.ro>
2023-06-28 14:29:16 +0300

    Patch LDAP Group object to bypass fetching to all users.

--- emrt/necd/content/patches.py
+++ emrt/necd/content/patches.py
@@ -1,13 +1,11 @@
 """Patches."""
 
-import hashlib
 from logging import getLogger
+from typing import List
 
 from Products.CMFDiffTool import dexteritydiff
 
-from plone.memoize.ram import cache
-
-EXCLUDED_FIELDS = list(dexteritydiff.EXCLUDED_FIELDS)
+EXCLUDED_FIELDS: List[str] = list(dexteritydiff.EXCLUDED_FIELDS)
 EXCLUDED_FIELDS.append("ghg_estimations")
 dexteritydiff.EXCLUDED_FIELDS = tuple(EXCLUDED_FIELDS)
 
@@ -15,26 +13,9 @@ log = getLogger(__name__)
 log.info("Patching difftool excluded fields to add ghg_estimations")
 
 
-def _sha_cachekey(sig):
-    return hashlib.sha256(str(sig).encode()).hexdigest()
-
-
-def _cachekey_LDAPSession_search(meth, self, *args, **kwargs):
-    kw = tuple([(k, v) for k, v in kwargs.items()])
-    sig = (meth.__name__, self.__name__, args, kw)
-    return _sha_cachekey(sig)
-
-
-@cache(_cachekey_LDAPSession_search)
-def LDAPSession_search_cache_wrapper(meth, *args, **kwargs):
-    """Wrapper."""
-    result = meth(*args, **kwargs)
-    return result
-
-
-def LDAPSession_search(self, *args, **kwargs):
-    """Patch LDAPSession.search."""
-    try:
-        return LDAPSession_search_cache_wrapper(self._old_search, *args, **kwargs)
-    except ValueError as exc:
-        return exc.message
+@property
+def Group_existing_member_ids(self):
+    """Patched Group.existing_member_ids."""
+    return self.translate_ids(
+        self.context.attrs.get(self._member_attribute, list())
+    )
