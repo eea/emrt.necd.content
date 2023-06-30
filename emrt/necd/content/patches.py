4153101eb8409aa35c1428cf4696f3d825d71af7
David Batranu <david.batranu@eaudeweb.ro>
2023-06-30 17:05:04 +0300

    Try to cache another ldap search method.

--- emrt/necd/content/patches.py
+++ emrt/necd/content/patches.py
@@ -2,6 +2,9 @@
 
 from logging import getLogger
 from typing import List
+import hashlib
+
+from plone.memoize.ram import cache
 
 from Products.CMFDiffTool import dexteritydiff
 
@@ -11,6 +14,10 @@ dexteritydiff.EXCLUDED_FIELDS = tuple(EXCLUDED_FIELDS)
 
 log = getLogger(__name__)
 log.info("Patching difftool excluded fields to add ghg_estimations")
+
+
+def _sha_cachekey(sig):
+    return hashlib.sha256(str(sig).encode()).hexdigest()
 
 
 @property
@@ -30,3 +37,20 @@ def LDAPUserPropertySheet__init__(self, principal, plugin):
     fullname = self._properties.get("fullname", "")
     if isinstance(fullname, (list, tuple)):
         self._properties["fullname"] = " ".join(fullname)
+
+
+def _cachekey(meth, self, *args, **kwargs):
+    kw = tuple([(k, v) for k, v in kwargs.items()])
+    sig = (meth.__name__, self.__name__, args, kw)
+    return _sha_cachekey(sig)
+
+
+@cache(_cachekey)
+def cache_wrapper(meth, *args, **kwargs):
+    """Wrapper."""
+    return meth(*args, **kwargs)
+    
+
+def LDAPPrincipals_search(self, *args, **kwargs):
+    """Patch LDAPPrincipals.search."""
+    return cache_wrapper(self._old_search, *args, **kwargs)
