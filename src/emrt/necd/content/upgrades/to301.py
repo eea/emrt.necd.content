0ca70546fe0fbaa82dcb224601e2646abf8f2eef
David Batranu <david.batranu@eaudeweb.ro>
2025-04-23 13:07:59 +0300

    [refs #260930] Commit every 10 observations.

--- src/emrt/necd/content/upgrades/to301.py
+++ src/emrt/necd/content/upgrades/to301.py
@@ -1,19 +1,24 @@
 import logging
 
-import five.intid.intid
 import plone.api as api
+import transaction
 
 from emrt.necd.content.browser.carryover import _replace_uuid, catalog_with_children
 
 logger = logging.getLogger(__name__)
 
+
 def fix_carried_over_observations(folder):
     catalog = api.portal.get_tool("portal_catalog")
+    count = 0
     for observation in folder.listFolderContents({"portal_type": "Observation"}):
         if hasattr(observation, "carryover_from"):
+            count += 1
             logger.info("Fixing %s", observation.absolute_url(1))
             _replace_uuid(observation)
             catalog_with_children(catalog, observation)
+            if count % 10 == 0:
+                transaction.savepoint(optimistic=True)
 
 
 def run(_):
