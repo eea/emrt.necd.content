b82f48b3966f2a8f06bb60a7213dd132355a3032
David Batranu <david.batranu@eaudeweb.ro>
2025-05-20 15:30:09 +0300

    [refs #287901] Add change_history view for managers.

---
+++ src/emrt/necd/content/browser/management.py
@@ -0,0 +1,186 @@
+from copy import deepcopy
+from functools import partial
+from pprint import pformat
+
+from zope.interface import Interface
+
+from zope.schema.vocabulary import SimpleVocabulary
+
+from z3c.form import button
+from z3c.form import form, field
+from plone.z3cform.layout import wrap_form
+from plone import schema
+from plone.autoform.form import AutoExtensibleForm
+from plone.supermodel import model
+from plone.supermodel.directives import fieldset
+
+from DateTime import DateTime
+from Products.Five.browser import BrowserView
+
+import plone.api as api
+
+from emrt.necd.content.browser.carryover import catalog_with_children
+
+
+class ReindexObservation(BrowserView):
+    def __call__(self):
+        catalog = api.portal.get_tool("portal_catalog")
+        catalog_with_children(catalog, self.context)
+        return self.request.RESPONSE.redirect(self.context.absolute_url())
+
+
+def cmp_datetime(cmp_date, entry):
+    return cmp_date - entry["time"]
+
+
+class IChangeHistoryForm(model.Schema):
+
+    fieldset(
+        "search",
+        label="Search",
+        fields=(
+            "wf",
+            "s_action",
+            "s_actor",
+            "s_author",
+            "s_object",
+            "s_review_state",
+            "s_role",
+            "s_state",
+            "s_time",
+        ),
+    )
+    wf = schema.Choice(
+        title="Workflow id",
+        values=[],
+    )
+    s_action = schema.Choice(title="Action", values=[], required=False)
+    s_actor = schema.Choice(title="Actor", values=[], required=False)
+    s_author = schema.Choice(title="Author", values=[], required=False)
+    s_object = schema.Choice(title="Object", values=[], required=False)
+    s_review_state = schema.Choice(
+        title="Review state", values=[], required=False
+    )
+    s_role = schema.Choice(title="Role", values=[], required=False)
+    s_state = schema.Choice(title="State", values=[], required=False)
+    s_time = schema.Choice(title="Time", values=[], required=False)
+
+    fieldset(
+        "replace",
+        label="Replace",
+        fields=(
+            "w_action",
+            "w_actor",
+            "w_author",
+            "w_object",
+            "w_review_state",
+            "w_role",
+            "w_state",
+            "w_time",
+        ),
+    )
+    w_action = schema.TextLine(title="Action", required=False)
+    w_actor = schema.TextLine(title="Actor", required=False)
+    w_author = schema.TextLine(title="Author", required=False)
+    w_object = schema.TextLine(title="Object", required=False)
+    w_review_state = schema.TextLine(title="Review state", required=False)
+    w_role = schema.TextLine(title="Role", required=False)
+    w_state = schema.TextLine(title="State", required=False)
+    w_time = schema.TextLine(title="Time", required=False)
+
+
+def vocabulary_from_history_items(items, name):
+    return SimpleVocabulary.fromValues(
+        sorted([x for x in {x.get(name, None) for x in items} if x])
+    )
+
+
+class ChangeHistoryForm(AutoExtensibleForm, form.Form):
+    schema = IChangeHistoryForm
+    ignoreContext = True
+
+    def update(self):
+        super(ChangeHistoryForm, self).update()
+        wf_values = list(self.context.workflow_history.values())
+        if wf_values:
+            wf_values = wf_values[0]
+
+        for group in self.groups:
+            for fname in group.fields:
+                if fname == "wf":
+                    group.fields[fname].field.vocabulary = (
+                        SimpleVocabulary.fromValues(
+                            list(self.context.workflow_history.keys())
+                        )
+                    )
+
+                elif fname.startswith("s_"):
+                    group.fields[fname].field.vocabulary = (
+                        vocabulary_from_history_items(wf_values, fname[2:])
+                    )
+        super(ChangeHistoryForm, self).update()
+
+    def updateWidgets(self):
+        super(ChangeHistoryForm, self).updateWidgets()
+
+    @button.buttonAndHandler("Save")
+    def handleSave(self, action):
+        params, errors = self.extractData()
+        if errors:
+            return False
+
+        search_params = {
+            k[2:]: v
+            for k, v in params.items()
+            if v is not None and k.startswith("s_")
+        }
+        search_time = search_params.pop("time", None)
+        if search_time:
+            search_time = DateTime(search_time)
+
+        write_params = {
+            k[2:]: v
+            for k, v in params.items()
+            if v is not None and k.startswith("w_")
+        }
+        if write_params.get("time"):
+            write_params["time"] = DateTime(write_params["time"])
+
+        found = []
+
+        for entry in self.context.workflow_history[params["wf"]]:
+            if all([entry[k] == v for k, v in search_params.items()]):
+                found.append(entry)
+
+        if search_time:
+            found.sort(key=partial(cmp_datetime, search_time))
+
+        if found:
+            target = found[0]
+            old_values = deepcopy(dict(target))
+
+            for k, v in write_params.items():
+                target[k] = v
+
+            self.context.workflow_history._p_changed = 1
+            self.context._p_changed = 1
+            self.context.reindexObject()
+
+            api.portal.show_message(
+                f"Modified {pformat(old_values)} => {pformat(dict(target))}"
+            )
+
+        else:
+            api.portal.show_message(
+                f"Nothing modified for query: {pformat(params)}"
+            )
+
+        self.request.response.redirect(self.context.absolute_url())
+
+    @button.buttonAndHandler("Cancel")
+    def handleCancel(self, action):
+        question = self.context.aq_parent
+        self.request.response.redirect(self.context.absolute_url())
+
+
+ChangeHistoryView = wrap_form(ChangeHistoryForm)
