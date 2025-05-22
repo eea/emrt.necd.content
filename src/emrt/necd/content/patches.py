2d9edf7fc904623a0626df93557ea78384738569
David Batranu <david.batranu@eaudeweb.ro>
2025-05-22 13:04:56 +0300

    [refs #288106] Monkey patch richtext widget.
    Fix error caused by Products.CMFPlone.patches.z3c_form because the rich
    text widget is "duplicated" without the self.form property.

--- src/emrt/necd/content/patches.py
+++ src/emrt/necd/content/patches.py
@@ -1,10 +1,14 @@
 """Patches."""
 
+from lxml import etree
+import json
+
 import hashlib
 from logging import getLogger
 from typing import List
 
 from Products.CMFDiffTool import dexteritydiff
+from plone.app.z3cform.widgets.richtext import RichTextWidgetBase
 
 from plone.memoize.ram import cache
 
@@ -38,13 +42,12 @@ def LDAPUserPropertySheet__init__(self, principal, plugin):
     if isinstance(fullname, (list, tuple)):
         self._properties["fullname"] = " ".join(fullname)
 
+
 def _items_from_dict(d):
     if isinstance(d, dict):
-        return tuple([
-            (k, _items_from_dict(v)) 
-            for k, v in d.items()
-        ])
+        return tuple([(k, _items_from_dict(v)) for k, v in d.items()])
     return d
+
 
 def _cachekey(meth, self, *args, **kwargs):
     kw = _items_from_dict(kwargs)
@@ -62,3 +65,67 @@ def LDAPPrincipals_search(self, *args, **kwargs):
     """Patch LDAPPrincipals.search."""
     result = cache_wrapper(self._old_search, *args, **kwargs)
     return result
+
+
+def RichTextWidget_render_input_mode(self):
+    # MODE "INPUT"
+    rendered = ""
+    allowed_mime_types = self.allowedMimeTypes()
+    if not allowed_mime_types or len(allowed_mime_types) <= 1:
+        # Display textarea with default widget
+        rendered = super().render()
+    else:
+        # Let pat-textarea-mimetype-selector choose the widget
+
+        # create a copy of RichTextWidget
+        textarea_widget = RichTextWidgetBase(self.request)
+        textarea_widget.field = self.field
+        textarea_widget.name = self.name
+        textarea_widget.value = self.value
+        textarea_widget.form = self.form
+
+        mt_pattern_name = "{}{}".format(
+            self._klass_prefix,
+            "textareamimetypeselector",
+        )
+
+        # Initialize mimetype selector pattern
+        # TODO: default_mime_type returns 'text/html', regardless of
+        # settings. fix in plone.app.textfield
+        value_mime_type = (
+            self.value.mimeType if self.value else self.field.default_mime_type
+        )
+        mt_select = etree.Element("select")
+        mt_select.attrib["id"] = f"{self.id}_text_format"
+        mt_select.attrib["name"] = f"{self.name}.mimeType"
+        mt_select.attrib["class"] = f"form-select {mt_pattern_name}"
+        mt_select.attrib[f"data-{mt_pattern_name}"] = json.dumps(
+            {
+                "textareaName": self.name,
+                "widgets": {
+                    "text/html": {  # TODO: currently, we only support
+                        # richtext widget config for
+                        # 'text/html', no other mimetypes.
+                        "pattern": self.pattern,
+                        "patternOptions": self.get_pattern_options(),
+                    },
+                },
+            },
+        )
+
+        # Create a list of allowed mime types
+        for mt in allowed_mime_types:
+            opt = etree.Element("option")
+            opt.attrib["value"] = mt
+            if value_mime_type == mt:
+                opt.attrib["selected"] = "selected"
+            opt.text = mt
+            mt_select.append(opt)
+
+        # Render the combined widget
+        textarea_widget.update()
+        rendered = "{}\n{}".format(
+            textarea_widget.render(),
+            etree.tostring(mt_select, encoding="unicode"),
+        )
+    return rendered
